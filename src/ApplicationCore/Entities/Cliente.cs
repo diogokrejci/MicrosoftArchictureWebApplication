8e46af19d8d666c9de42969ac80a756d6fe99a63
Diogo Krejci <diogokrejci@petrobtas.com.br>
2019-04-09 11:22:13 -0300

    Add project files.

---
+++ src/ApplicationCore/Entities/Cliente.cs
@@ -0,0 +1,16 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            
+        }
+
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+    }
+}
